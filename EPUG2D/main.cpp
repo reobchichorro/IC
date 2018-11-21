416d55da3e9d3aa71680ab56f85e61af98d0417d
Rodrigo Chichorro <reobcnegf@gmail.com>
2018-11-21 09:44:42 -0200

    Edição na main

--- EPUG2D/main.cpp
+++ EPUG2D/main.cpp
@@ -23,9 +23,26 @@ int main() {
     for(int i=0; i<resolution; i++)
         grid[i].resize(resolution);
 
+    vector<pair<int,int> > cell0(map0.size());
     #pragma omp parallel for
     for(int i=0; i<map0.size(); i++) {
-        
+        //determine cell for each edge of map0
+    }
+
+    for(int i=0; i<map0.size(); i++) {
+        //grid[cell0[i].first][cell0[i].second].first.push_back()
+        //Insert edges of map0 in grid
+    }
+
+    vector<pair<int,int> > cell1(map1.size());
+    #pragma omp parallel for
+    for(int i=0; i<map1.size(); i++) {
+        //determine cell for each edge of map1
+    }
+
+    for(int i=0; i<map1.size(); i++) {
+        //grid[cell1[i].first][cell1[i].second].second.push_back()
+        //Insert edges of map1 in grid
     }
 
     return 0;

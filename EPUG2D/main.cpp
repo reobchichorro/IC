59950690f2e5468887357405afb25db952997ca9
Rodrigo Chichorro <reobcnegf@gmail.com>
2018-12-12 08:44:48 -0200

    Encontrando a célula de cada aresta. Ainda não finalizado.

--- EPUG2D/main.cpp
+++ EPUG2D/main.cpp
@@ -9,6 +9,7 @@ typedef double T;
 typedef Edge<T> E;
 typedef vector<E> ve;
 typedef pair<T,T> ptt;
+typedef pair<int,int> pii;
 
 void getX(const ve& map0, const ve& map1, ptt& x){
     x.first = map0[0].getInit().getX();
@@ -46,6 +47,17 @@ void getY(const ve& map0, const ve& map1, ptt& y){
     }
 }
 
+void findCell(const ptt& x, const ptt& y, const ptt& cellSize, pii& cell, const int resolution, const E& edgeFromMap) {
+    for(int i=0; i<resolution; i++)
+        for(int j=0; j<resolution; j++) {
+            cell = make_pair(i,j);
+            if(edgeFromMap.getInit().getX() - (x.first + i*cellSize.first) > cellSize.first)
+                break;
+            if(edgeFromMap.getInit().getY() - (y.first + j*cellSize.second) <= cellSize.second)
+                return;
+        }
+}   // Uma versao usando busca binaria seria melhor
+
 int main() {
     int resolution = 10;
     ifstream m0("map0.txt"); ifstream m1("map1.txt"); 
@@ -64,28 +76,34 @@ int main() {
     for(int i=0; i<resolution; i++)
         grid[i].resize(resolution);
 
-    ptt x; getX(map0,map1,x);
-    ptt y; getY(map0,map1,y);
+    ptt x; getX(map0,map1,x); x.second += 0.5; // x.first e' o limite inferior da coordenada x da grade e x.second e' o limite superior.
+    ptt y; getY(map0,map1,y); y.second += 0.5; // Analogo para y.
 
-    cout << x.first << " " << y.first << "\n";
-    cout << x.second << " " << y.second << "\n";
-    vector<pair<int,int> > cell0(map0.size());
+    cerr << x.first << " " << y.first << "\n";
+    cerr << x.second << " " << y.second << "\n";
+    vector<pii> cell0(map0.size());
+    ptt cellSize; cellSize.first = (x.second - x.first)/resolution; cellSize.second = (y.second - y.first)/resolution;
+    cerr << cellSize.first << " x " << cellSize.second << "\n\n";
+
     // #pragma omp parallel for
     for(int i=0; i<map0.size(); i++) {
         //determine cell for each edge of map0
-        
+        findCell(x,y,cellSize,cell0[i],resolution,map0[i]);
+        cerr << map0[i].getInit().getX() << " " << map0[i].getInit().getY() << " - " << cell0[i].first << " " << cell0[i].second << "\n";
     }
 
     // for(int i=0; i<map0.size(); i++) {
     //     //grid[cell0[i].first][cell0[i].second].first.push_back()
     //     //Insert edges of map0 in grid
     // }
-
-    // vector<pair<int,int> > cell1(map1.size());
+    cerr << "\n";
+    vector<pii> cell1(map1.size());
     // #pragma omp parallel for
-    // for(int i=0; i<map1.size(); i++) {
-    //     //determine cell for each edge of map1
-    // }
+    for(int i=0; i<map1.size(); i++) {
+        //determine cell for each edge of map1
+        findCell(x,y,cellSize,cell1[i],resolution,map1[i]);
+        cerr << map1[i].getInit().getX() << " " << map1[i].getInit().getY() << " - " << cell1[i].first << " " << cell1[i].second << "\n";
+    }
 
     // for(int i=0; i<map1.size(); i++) {
     //     //grid[cell1[i].first][cell1[i].second].second.push_back()
